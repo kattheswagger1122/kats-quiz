6e2f6beab6c549ec23494fe0dbb2bb74c59fc126
Katrina  Shih <katrinashih@Katrinas-MacBook-Pro.local>
2022-05-15 23:55:11 -0400

    code

---
+++ scriptQ.js
@@ -0,0 +1,138 @@
+
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What is Katrina's Favorite Bread?",
+    answer: "Croissant",   
+    options: [
+        "Croissant",
+        "Sourdough",
+        "Pineapple Bread",
+        "Pita"   
+    ]
+  },
+
+    {
+    numb: 2,
+    question: "What is Kat's Favorite Clothing Brand as of May 2022?",
+    answer: "Theory",    
+    options: [
+    "COS",
+    "Theory",
+    "Oak+Fort",
+    "Uniqlo"
+       ]
+  },
+
+    {
+    numb: 3,
+    question: "What instrument does Kat play (well lol)?", 
+    answer: "flute",
+    options: [
+    "piano",
+    "violin",
+    "recorder",
+    "flute"
+    ]
+  },
+
+    {
+    numb: 4,
+    question: "What Is Kat's Favorite Music Genre?",
+    answer: "Jpop",
+    options: [
+        "Kpop",
+        "Jpop",
+        "Metal",
+        "Indie"
+    ]
+  },
+
+    {
+    numb: 5,
+    question: "What is Kat's go-to Boba Order?",
+    answer: "Yakult Green Tea",
+    options: [ 
+    "Classic Bubble Tea",
+    "Brown Sugar Milk",
+    "Strawberry Green Tea with Lychee Jelly",
+    "Yakult Green Tea"
+    ]
+  },
+  
+    {
+    numb: 6,
+    question: "What is Kat's go-to restaurant in Chinatown NYC?",
+    answer: "Spring Cafe on Canal Street",    
+    options: [
+    "Go Go Dim Sum",
+    "The Original Bodai Restaurant",
+    "Spring Cafe on Canal Street",
+    "Happy Veggie Restaurant"
+    ]
+    },
+
+    {
+    numb: 7,
+    question: "Kat's favorite dish?",
+    answer: "All Of The Above",
+    options: [
+    "Pasta (ANY TYPE)", 
+    "Ramen", 
+    "Mandy's Salad with Sesame Dressing", 
+    "All Of The Above"
+    ]
+    },
+
+    {
+    numb: 8,
+    question: "Kat's Favorite Ghibli Animation Film",
+    answer: "Howl's Moving Castle",
+    options: [
+    "Spirited Away", 
+    "Ponyo", 
+    "Howl's Moving Castle", 
+    "Castle in the Sky"
+    ]
+    },
+    
+    {
+    numb: 9,
+    question: "How Many Languages Does Kat Know Fluently",
+    answer: "4",
+    options: [
+    "3", 
+    "4", 
+    "5", 
+    "2"
+    ]
+    },
+
+    {
+    numb: 10,
+    question: "What is Kat's Chinese Zodiac Sign",
+    answer: "Tiger",
+    options: [
+    "Rabbit", 
+    "Tiger", 
+    "Ox", 
+    "Dragon"
+    ]
+    },
+
+    {
+    numb: 11,
+    question: "What is Kat's Spirit Animal",
+    answer: "Koala",
+    options: [
+    "Dolphin", 
+    "Cheeta", 
+    "Koala", 
+    "Blobfish"
+    ]
+    }
+
+];
+
+
